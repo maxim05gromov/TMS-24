1b83cfd061726fa368cb9631e7040c2547a6ba85
Maksim <i@mgromov.ru>
2024-09-23 16:22:37 +0300

    Initial Commit

---
+++ HW24/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW24
+//
+//  Created by Максим Громов on 23.09.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW24/HW24App.swift
@@ -0,0 +1,17 @@
+//
+//  HW24App.swift
+//  HW24
+//
+//  Created by Максим Громов on 23.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct HW24App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
